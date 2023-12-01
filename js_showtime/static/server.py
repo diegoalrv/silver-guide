9b1a4249c30fa9f2a9d8a1c31b31fa356c89db46
diegoalrv <d.alexis.ramirez@gmail.com>
2023-12-01 02:37:55 -0300

    modified index and main

---
+++ js_showtime/static/server.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+from pathlib import Path
+
+app = FastAPI()
+
+# Sirve archivos estáticos desde la carpeta 'src'
+app.mount("/static", StaticFiles(directory="src"), name="static")
+
+@app.get("/assets/{file_path:path}")
+async def get_glb(file_path: str):
+    file_location = Path("src/assets") / Path(file_path)
+    if file_location.exists() and file_location.suffix == ".glb":
+        return FileResponse(file_location, media_type="model/gltf-binary")
+    return FileResponse(file_location)  # Puedes manejar archivos no encontrados o tipos de archivo incorrectos según sea necesario
--- js_showtime/static/src/main.js
+++ js_showtime/static/src/main.js
@@ -1,12 +1,10 @@
 import * as THREE from 'three';
-import modelUrl from './assets/Ceramic_pot_model.glb'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('myCanvas') });
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -14,12 +12,12 @@ document.body.appendChild(renderer.domElement);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // color y intensidad
 scene.add(ambientLight);
 
-// const loader = new GLTFLoader();
-// loader.load(modelUrl, function (gltf) {
-//   scene.add(gltf.scene);
-// }, undefined, function (error) {
-//   console.error(error);
-// });
+const loader = new GLTFLoader();
+loader.load('/static/assets/Ceramic_pot_model.glb', function (gltf) {
+  scene.add(gltf.scene);
+}, undefined, function (error) {
+  console.error(error);
+});
 
 // Crear la geometría del cubo
 const geometry = new THREE.BoxGeometry(1, 1, 1); // dimensiones del cubo: ancho, alto, profundidad
@@ -33,7 +31,7 @@ const cube = new THREE.Mesh(geometry, material);
 // Añadir el cubo a la escena
 scene.add(cube);
 
-camera.position.z = 1;
+camera.position.z = 10;
 
 function animate() {
   requestAnimationFrame(animate);
